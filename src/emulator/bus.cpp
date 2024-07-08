48d51d37ccfd44f8ffc835ea2142cc95c11dd9c2
Jack Brewer <jack_Brewer96@hotmail.com>
2024-07-08 14:38:37 +0100

    Bus Update

--- src/emulator/bus.cpp
+++ src/emulator/bus.cpp
@@ -22,66 +22,66 @@ void bus::connectCPU(std::shared_ptr<cpu> cpu)
 	m_cpu = cpu;
 }
 
-uint8_t bus::read8bit(uint16_t address)
+uint8_t bus::read(uint16_t address,int bits)
 {
-	if (address < 0x8000)
+	if (bits == 8)
 	{
-		//ROM Data
-		return m_loader->readCartridge(address);
-	}
-	else if (address < 0xA000)
-	{
-		//Character map data
+		if (address < 0x8000)
+		{
+			//ROM Data
+			return m_loader->readCartridge(address);
+		}
+		else if (address < 0xA000)
+		{
+			//Character map data
 
-	}
-	else if (address < 0xC000)
-	{
-		//Cartridge RAM
-		return m_loader->readCartridge(address);
-	}
-	else if (address < 0xE000)
-	{
-		//Working RAM
+		}
+		else if (address < 0xC000)
+		{
+			//Cartridge RAM
+			return m_loader->readCartridge(address);
+		}
+		else if (address < 0xE000)
+		{
+			//Working RAM
 
-	}
-	else if (address < 0xFE00)
-	{
-		//reserved echo ram
-		return 0;
+		}
+		else if (address < 0xFE00)
+		{
+			//reserved echo ram
+			return 0;
 
-	}
-	else if (address < 0xFEA0)
-	{
-		//DAM
+		}
+		else if (address < 0xFEA0)
+		{
+			//DAM
 
-	}
-	else if (address < 0xFF00)
-	{
-		//reserved memory
-		return 0;
-	}
-	else if (address < 0xFF80)
-	{
-		//IO
+		}
+		else if (address < 0xFF00)
+		{
+			//reserved memory
+			return 0;
+		}
+		else if (address < 0xFF80)
+		{
+			//IO
 
+		}
+		else if (address < 0xFFFF)
+		{
+			//CPU Interrupt enable register
+			m_cpu->getIERegister();
+		}
+		return m_memory->read(address);
 	}
-	else if (address < 0xFFFF)
+	else if (bits == 16)
 	{
-		//CPU Interrupt enable register
-		m_cpu->getIERegister();
+		uint16_t lo = read(address);
+		uint16_t hi = read(address + 1);
+
+		return lo | (hi << 8);
 	}
-	return m_memory->read(address);
 }
-
-uint16_t bus::read16bit(uint16_t address)
-{
-	uint16_t lo = read8bit(address);
-	uint16_t hi = read8bit(address + 1);
-
-	return lo | (hi << 8);
-
-}
-
 void bus::write(uint16_t address, uint8_t value)
 {
 	if (address < 0x8000)
--- src/emulator/bus.h
+++ src/emulator/bus.h
@@ -11,8 +11,7 @@ public:
 	bus(std::shared_ptr <cartridgeLoader> loader, std::shared_ptr <memory> memory);
 	bus(std::shared_ptr <cartridgeLoader> loader, std::shared_ptr <memory> memory, std::shared_ptr <cpu> cpu);
 	void connectCPU(std::shared_ptr <cpu> cpu);
-	uint8_t read8bit(uint16_t address);
-	uint16_t read16bit(uint16_t address);
+	uint8_t read(uint16_t address, int bits = 8);
 	void write(uint16_t address, uint8_t value);
 	void write(uint16_t address, uint16_t value);
 private:
