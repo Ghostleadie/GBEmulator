aeb6282f1150a95aad7b3c467c852fce6c31e20e
Jack Brewer <jack_Brewer96@hotmail.com>
2025-09-13 22:13:12 +0100

    Bus Update

--- src/components/bus.cpp
+++ src/components/bus.cpp
@@ -6,10 +6,9 @@
 #include "../Utility/utility.h"
 #include "cartridgeLoader.h"
 
-void bus::connectComponents(const std::shared_ptr<cartridgeLoader>& loader, const std::shared_ptr<apu>& apu, const std::shared_ptr<joypad>& joypad, const std::shared_ptr<ppu>& ppu, const std::shared_ptr<timer>& timer)
+void bus::connectComponents(const std::shared_ptr<cartridgeLoader>& loader, const std::shared_ptr<joypad>& joypad, const std::shared_ptr<ppu>& ppu, const std::shared_ptr<timer>& timer)
 {
 	m_cartridge = loader;
-	m_apu = apu;
 	m_joypad = joypad;
 	m_ppu = ppu;
 	m_timer = timer;
@@ -30,7 +29,8 @@ uint8_t bus::read(uint16_t address)
 	else if (address < 0xA000)
 	{
 		//Character map data
-
+		LOG_INFO("Bus Read from Character map data: {:04X}", address);
+		return 0;
 	}
 	else if (address < 0xC000)
 	{
@@ -40,46 +40,54 @@ uint8_t bus::read(uint16_t address)
 	else if (address < 0xE000)
 	{
 		//Working RAM
-
+		return readWRam(address);
 	}
 	else if (address < 0xFE00)
 	{
 		//reserved echo ram
+		LOG_INFO("Bus Read from echo ram: {:04X}", address);
 		return 0;
 
 	}
 	else if (address < 0xFEA0)
 	{
 		//DAM
+		LOG_INFO("Bus Read from DAM: {:04X}", address);
 		return 0x0;
 	}
 	else if (address < 0xFF00)
 	{
 		//reserved memory
+		LOG_INFO("Bus Read from Reserve memory: {:04X}", address);
 		return 0;
 	}
 	else if (address < 0xFF80)
 	{
 		//IO
+		LOG_INFO("Bus Read from I/o: {:04X}", address);
 		return 0;
 	}
 	else if (address < 0xFFFF)
 	{
+		LOG_INFO("Bus Read from Interrupt enable register: {:04X}", address);
 		//CPU Interrupt enable register
 		//m_cpu->getIERegister();
 	}
-	if (utility::inRange(address, 0xC000, 0xCFFF))
-	{
-		return wram.at(address - 0xC000);
-	}
-	else if (utility::inRange(address, 0xFF80, 0xFFFE))
-	{
-		return hram.at(address - 0xFF80);
-	}
 	else
 	{
-		LOG_ERROR("invalid memory read");
-		return 0;
+		if (utility::inRange(address, 0xC000, 0xCFFF))
+		{
+			return readWRam(address);
+		}
+		else if (utility::inRange(address, 0xFF80, 0xFFFE))
+		{
+			return readHRam(address);
+		}
+		else
+		{
+			LOG_ERROR("invalid memory read");
+			return 0;
+		}
 	}
 	return 0;
 }
@@ -106,7 +114,7 @@ void bus::write(uint16_t address, uint8_t value)
 	else if (address < 0xE000)
 	{
 		//Working RAM
-		//m_memory->write(address, value);
+		writeWRam(address, value);
 	}
 	else if (address < 0xFE00)
 	{
@@ -139,12 +147,12 @@ void bus::write(uint16_t address, uint8_t value)
 	{
 		if (utility::inRange(address, 0xC000, 0xCFFF))
 		{
-			wram.at(address - 0xC000) = value;
+			writeWRam(address,value);
 			return;
 		}
 		else if (utility::inRange(address, 0xFF80, 0xFFFE))
 		{
-			hram.at(address - 0xFF80) = value;
+			writeHRam(address,value);
 			return;
 		}
 		else
@@ -153,4 +161,43 @@ void bus::write(uint16_t address, uint8_t value)
 			return;
 		}
 	}
+}
+
+uint8_t bus::readWRam(uint16_t address)
+{
+
+	address -= 0xC000;
+
+	if (address >= 0x2000) {
+		LOG_ERROR("WRam read out of bounds: {}", address);
+		exit(-1);
+	}
+
+	return wRam[address];
+}
+
+void bus::writeWRam(uint16_t address, const uint8_t value)
+{
+	address -= 0xC000;
+
+	if (address >= 0x2000)
+	{
+		LOG_ERROR("WRam write out of bounds: {}", address);
+		exit(-1);
+	}
+	wRam[address] = value;
+}
+
+uint8_t bus::readHRam(uint16_t address)
+{
+	address -= 0xFF80;
+
+	return hRam[address];
+}
+
+void bus::writeHRam(uint16_t address, const uint8_t value)
+{
+	address -= 0xFF80;
+
+	hRam[address] = value;
 }
--- src/components/bus.h
+++ src/components/bus.h
@@ -10,7 +10,6 @@
 class timer;
 class ppu;
 class joypad;
-class apu;
 class cartridgeLoader;
 
 class bus :   public memorycomponentMessanger
@@ -18,20 +17,26 @@ class bus :   public memorycomponentMessanger
 public:
 	bus()= default;
 	void addComponent(const std::string& name, const std::shared_ptr<memoryComponent> &component);
-	void connectComponents(const std::shared_ptr<cartridgeLoader>& loader, const std::shared_ptr<apu>& apu, const std::shared_ptr<joypad>& joypad, const std::shared_ptr<ppu>& ppu, const std::shared_ptr<timer>& timer);
+	void connectComponents(const std::shared_ptr<cartridgeLoader>& loader, const std::shared_ptr<joypad>& joypad, const std::shared_ptr<ppu>& ppu, const std::shared_ptr<timer>& timer);
 	void notify(const std::string& event, std::shared_ptr<memoryComponent> sender) override;
 
 	uint8_t read(uint16_t address) override;
 	void write(uint16_t address, uint8_t value) override;
+
+	uint8_t readWRam(uint16_t address);
+	void writeWRam(uint16_t address, uint8_t value);
+
+	uint8_t readHRam(uint16_t address);
+	void writeHRam(uint16_t address, uint8_t value);
+
 private:
 	std::shared_ptr<cartridgeLoader> m_cartridge;
-	std::shared_ptr<apu> m_apu;
 	std::shared_ptr<joypad> m_joypad;
 	std::shared_ptr<ppu> m_ppu;
 	std::shared_ptr<timer> m_timer;
 
-	std::array<uint8_t, 0x2000> wram{};
-	std::array<uint8_t, 0x7F> hram{};
+	std::array<uint8_t, 0x2000> wRam{};
+	std::array<uint8_t, 0x80> hRam{};
 };
 
 
