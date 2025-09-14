576fe618ca087ff950c04002f67f741a992a131e
Jack Brewer <jack_Brewer96@hotmail.com>
2025-09-14 22:49:35 +0100

    Update cpuDebugUI.cpp

--- src/UI/debugpanels/cpuDebugUI.cpp
+++ src/UI/debugpanels/cpuDebugUI.cpp
@@ -10,9 +10,13 @@
 void cpuDebugUI::updateUI(cpu& cpu)
 {
 	ImGui::Begin("CPU Debug");
-	ImGui::Text("PC: %04X", cpu.getPC());
-	ImGui::Text("SP: %04X", cpu.getRegistersDebug().sp);
-	ImGui::Text("AF: %04X", cpu.getRegistersDebug().af);
+	ImGui::Text("PC: %04X  SP: %04X", cpu.getPC(), cpu.getRegistersDebug().sp);
+	ImGui::Text("A: %04X F: %04X AF: %04X", cpu.getRegistersDebug().a, cpu.getRegistersDebug().f, cpu.getRegistersDebug().af);
+	ImGui::Text("B: %04X C: %04X BC: %04X", cpu.getRegistersDebug().b, cpu.getRegistersDebug().c, cpu.getRegistersDebug().bc);
+	ImGui::Text("D: %04X E: %04X DE: %04X", cpu.getRegistersDebug().d, cpu.getRegistersDebug().e, cpu.getRegistersDebug().de);
+	ImGui::Text("H: %04X L: %04X HL: %04X", cpu.getRegistersDebug().h, cpu.getRegistersDebug().l, cpu.getRegistersDebug().hl);
+	ImGui::NewLine();
+	ImGui::Text("Flags: Z=%d N=%d H=%d C=%d", cpu.isZeroFlagSet(), cpu.isSubtractFlagSet(), cpu.isHalfCarryFlagSet(), cpu.isCarryFlagSet());
 
 	// Stepping toggle
 	static bool steppingEnabled = false;
