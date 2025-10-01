eef4329076a77beebc75e355e4887868a7696e31
Jack Brewer <jack_Brewer96@hotmail.com>
2025-10-01 19:02:09 +0100

    Create IClock.h

---
+++ src/interfaces/IClock.h
@@ -0,0 +1,18 @@
+﻿//
+// Created by Jack_ on 24/09/2025.
+//
+
+#ifndef GAMEBOYEMULATOR_ICLOCK_H
+#define GAMEBOYEMULATOR_ICLOCK_H
+#include <cstdint>
+
+
+class IClock
+{
+public:
+    virtual ~IClock() = default;
+    virtual void cycles(std::uint64_t cpuCycles) = 0;
+};
+
+
+#endif //GAMEBOYEMULATOR_ICLOCK_H
